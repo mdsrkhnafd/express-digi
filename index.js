90ec71e208f6e228bd1fea8896bf5f0962141084
Mudasir Khan <afridi4mudasir@gmail.com>
2025-01-21 17:01:30 +0500

    add dotenv support and update .gitignore

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import express from "express";
 
 // Create an Express app
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 // Parse JSON request bodies
 app.use(express.json());
 
